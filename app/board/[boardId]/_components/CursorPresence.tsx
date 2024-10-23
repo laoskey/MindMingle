477377226f777a6b8af79d39a8d9a94048d5083d
观澜听雨 <tangxiaochuane@yeah.net>
2024-10-23 15:56:55 +0800

    add the func that each teammate in the same room can see eachother's drawing

--- app/board/[boardId]/_components/CursorPresence.tsx
+++ app/board/[boardId]/_components/CursorPresence.tsx
@@ -5,11 +5,14 @@ import { memo } from "react";
 // import { useOthersConnectionIds } from "@liveblocks/react/suspense";
 import { Cursor } from "./Cursor";
 import { useOthersConnectionIds } from "@/liveblocks.config";
+import { shallow, useOthersMapped } from "@liveblocks/react/suspense";
+import { colorToCss } from "@/lib/utils";
+import { Path } from "./Path";
 // interface CursorPresenceProps {}
 
 const Cursors = () => {
   const ids = useOthersConnectionIds();
-  console.log(ids);
+
   return (
     <>
       {ids.map((connectionId) => {
@@ -24,10 +27,39 @@ const Cursors = () => {
   );
 };
 
+const Drafts = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      penciDraft: other.presence.penciDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.penciDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.penciDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : "#000"}
+            />
+          );
+        }
+      })}
+    </>
+  );
+  return null;
+};
+
 export const CursorPresence = memo(() => {
   return (
     <>
-      {/* TODO :Draft pencil */}
+      <Drafts />
       <Cursors />
     </>
   );
